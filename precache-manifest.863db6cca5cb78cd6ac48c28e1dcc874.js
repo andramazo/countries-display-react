6012e4a4281ed526b3b3ca70216a4b01479288c0
andramazo <anushreedave91@gmail.com>
2020-08-26 11:48:48 -0400

    Updates

---
+++ precache-manifest.863db6cca5cb78cd6ac48c28e1dcc874.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c3bd40421946da65daabc3b76beb44",
+    "url": "/countries-display-react/index.html"
+  },
+  {
+    "revision": "e3408065f85573a2f6e8",
+    "url": "/countries-display-react/static/css/4.303a66fb.chunk.css"
+  },
+  {
+    "revision": "ed96cf730e49da37b0b6",
+    "url": "/countries-display-react/static/css/5.3d991527.chunk.css"
+  },
+  {
+    "revision": "aa1e2cce30c74027c41b",
+    "url": "/countries-display-react/static/css/main.0f983260.chunk.css"
+  },
+  {
+    "revision": "e92724a1246b707053aa",
+    "url": "/countries-display-react/static/js/0.db56c03d.chunk.js"
+  },
+  {
+    "revision": "525458e132aa87373564",
+    "url": "/countries-display-react/static/js/3.b6219c3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/countries-display-react/static/js/3.b6219c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3408065f85573a2f6e8",
+    "url": "/countries-display-react/static/js/4.117c9ab1.chunk.js"
+  },
+  {
+    "revision": "ed96cf730e49da37b0b6",
+    "url": "/countries-display-react/static/js/5.dacb4f02.chunk.js"
+  },
+  {
+    "revision": "aa1e2cce30c74027c41b",
+    "url": "/countries-display-react/static/js/main.c28ef113.chunk.js"
+  },
+  {
+    "revision": "027cf0920a704ffe8ecd",
+    "url": "/countries-display-react/static/js/runtime-main.b37d6494.js"
+  }
+]);
